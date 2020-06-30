2cc5569f2f255c441ee02d4c91332f4938ca0ece
Steven Lee <steven.lee1393@gmail.com>
2020-06-30 21:18:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+		programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +81,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+		programmer_hash[:yukihiro_matsumoto]={:known_for => "Ruby", :languages => ["LISP", "C"]}
+	programmer_hash
 end
 
 def changing_alan
